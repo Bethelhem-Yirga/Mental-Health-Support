b974c87c40846b93c1911ce81a25bdc02e00963f
Bethelhem-Yirga <bethelhemyirga3@gmail.com>
2026-06-03 15:18:20 +0800

    backend/routes/therapistRoutes.js

--- backend/routes/therapistRoutes.js
+++ backend/routes/therapistRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getAllTherapists, getTherapistById } = require('../controllers/therapistController');
+const {
+  getAllTherapists,
+  getTherapistById
+} = require('../controllers/therapistController');
+const {
+  validateTherapistQuery,
+  validateTherapistId
+} = require('../middleware/validators');
 
-// Make sure these are defined correctly
-router.get('/', getAllTherapists);
-router.get('/:id', getTherapistById);
+router.get('/', validateTherapistQuery, getAllTherapists);
+router.get('/:id', validateTherapistId, getTherapistById);
 
-module.exports = router;  // Export the router, not an object
+module.exports = router;
